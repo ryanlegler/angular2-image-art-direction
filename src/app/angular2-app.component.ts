f98ef5235af9d4eeb961ce3723d259248a9f1b1c
Ryan Legler <ryan.legler@gmail.com>
2016-05-06 07:53:18 -0700

    first commit angular 2 app generated using angular2 CLI

--- src/app/angular2-app.component.ts
+++ src/app/angular2-app.component.ts
@@ -4,8 +4,9 @@ import { Component } from '@angular/core';
   moduleId: module.id,
   selector: 'angular2-app-app',
   templateUrl: 'angular2-app.component.html',
-  styleUrls: ['angular2-app.component.css']
+  styleUrls: ['scss/angular2-app.component.css']
 })
 export class Angular2AppAppComponent {
-  title = 'angular2-app works!';
+  title = 'hello!';
 }
+
